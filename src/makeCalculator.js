e1259dcf49a83cc57cbfca3e3aabc393f87261a9
oliveira-tech <oliveira.tech07@gmail.com>
2026-04-16 11:02:36 -0300

    Solution Calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,28 +4,37 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
-  return {
+  // ITEM #1: Criamos a variável com o nome correto
+  const calculator = {
     result: 0,
 
-    reset: function () {
-      this.result = 0;
-      return this;  
+    add(a, b) {
+      return a + b;
+    },
+    subtract(a, b) {
+      return a - b;
+    },
+    multiply(a, b) {
+      return a * b;
+    },
+    divide(a, b) {
+      return a / b;
     },
 
-    add: (a, b) => a + b,
+    reset() {
+      this.result = 0;
 
-    subtract: (a, b) => a - b,
-
-    multiply: (a, b) => a* b,
-
-    divide: (a, b) => a / b,
-
-    operate: function (callback, value) {
-      this.result = callback(this.result, value);
       return this;
-    }
-  }
+    },
+
+    operate(callback, value) {
+      this.result = callback(this.result, value);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
